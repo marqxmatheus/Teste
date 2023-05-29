c74f1bcd0f2f4c0ef3d3d14ea6cfcebb63476746
marqxmatheus <101539634+marqxmatheus@users.noreply.github.com>
2023-05-29 20:50:15 -0300

    Continuando OO

---
+++ exerciciosDeFixação/ex04/src/README.md
@@ -0,0 +1,3 @@
+## Calculando preço de compra de dólares
+
+Nesse programa é possível calcular o preço de uma determinada quantidade de dólares em reais levando em conta o IOF.
---
+++ exerciciosDeFixação/ex04/src/application/App.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.CurrencyConverter;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is the dollar price? ");
+        double dollarPrice  = sc.nextDouble();
+        System.out.print("How many dollar will be bought? ");
+        double amount = sc.nextDouble();
+        System.out.printf("Amount to be paid in reais:  %.2f", CurrencyConverter.dollarToReal(amount, dollarPrice));
+
+        sc.close();
+    }
+}
